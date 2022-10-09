a2f419e734b077ed1b09ece14809535d6440568f
Gocer615 <gtumayy21@gmail.com>
2022-10-09 08:30:38 +0300

    feat: x position touch settings done

---
+++ Assets/Scripts/XCamControl.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class XCamControl : MonoBehaviour
+{  
+    bool right;
+    bool left;
+
+    private void Update()
+    {
+        Vector3 go_right = new Vector3(1, transform.position.y, transform.position.z);
+        Vector3 go_left = new Vector3(-1, transform.position.y, transform.position.z);
+
+        if (Input.touchCount > 0)
+        {
+            Touch finger = Input.GetTouch(0);
+
+            if (finger.deltaPosition.x > 50.0f)
+            {
+                right = true;
+                left = false;
+            }
+
+            if (finger.deltaPosition.x < -50.0f)
+            {
+                right = false;
+                left = true;
+            }
+
+            if (right == true)
+            {
+                transform.position = Vector3.Lerp(transform.position, go_right, 5 * Time.deltaTime);
+            }
+
+            if (left == true)
+            {
+                transform.position = Vector3.Lerp(transform.position, go_left, 5 * Time.deltaTime);
+            }
+        }
+    }
+
+}
